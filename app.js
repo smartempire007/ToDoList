8853f79d69bbbc1ceb433d6d5de57f735b9f72c6
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2021-10-08 03:32:38 +0100

    another change

--- app.js
+++ app.js
@@ -173,8 +173,11 @@ app.post("/work", function(req, res) {
 
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(process.env.port || 3000, function() {
-    console.log("Server is running on port: 3000...");
+app.listen(port, function() {
+    console.log("Server has started successfully");
 });
